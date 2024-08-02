6f86c0eb7944b0da9f3b42c2eaaf7a133b717b4f
sbakunowicz <sbakunowicz@lbpro.pl>
2024-08-02 17:33:55 +0200

    Plan with odd and even

--- src/app/plan/page.tsx
+++ src/app/plan/page.tsx
@@ -1,4 +1,4 @@
-import "./plan.css";
+import "./newplan.css";
 import Image from "next/image";
 
 
@@ -7,91 +7,230 @@ export default function Plan() {
         <div className="w-full items-center flex flex-col">
             <div className="planBody">
                 <div className="planBody-content">
-                    <div className="timeline-container">
-                        <p className="timeline-header">Rozkład jazdy</p>
+                    <p className="timeline-header">Rozkład jazdy</p>
+                    <p className="timeline-day">Sobota</p>
+                    <div className="schedule">
+                        <div className="event">
+                            <div className="left">
 
-                        <p className="timeline-day">Sobota</p>
-                        <div className="timeline">
-                            <div className="timeline-event">
-                                <div className="icon"><Image src="/icons/pin.png" alt="Welcome Toast" width={10}
-                                                             height={10} unoptimized/>
-                                </div>
-                                <div className="time">14:00</div>
-                                <div className="description">Meldunek</div>
                             </div>
-                            <div className="timeline-event">
-                                <div className="icon"><Image src="/icons/toast.png" alt="Cake Cutting" width={10}
-                                                             height={10} unoptimized/>
+                            <div className="right info">
+                                <span className="dot"/>
+                                <div className="icon">
+                                    <Image src={"/icons/entry-pin.png"} alt="meldunek" unoptimized width={45}
+                                           height={45}/>
                                 </div>
-                                <div className="time">18:00</div>
-                                <div className="description">Drineczek</div>
+                                <div className="description">
+                                    <div className="time">14:00</div>
+                                    <span className="text">Meldunek</span>
+                                </div>
                             </div>
-                            <div className="timeline-event">
-                                <div className="icon"><Image src="/icons/ceremony.png" alt="First Dance" width={10}
-                                                             height={10} unoptimized/>
+                        </div>
+                        <div className="event">
+                            <div className="left info">
+                                <div className="description">
+                                    <div className="time">17:30</div>
+                                    <span className="text">Aperitivo</span>
                                 </div>
+                                <div className="icon">
+                                    <Image src={"/icons/toast.png"} alt="toast" unoptimized width={45} height={45}/>
+                                </div>
+                                <span className="dot"/>
+                            </div>
+                            <div className="right">
+
+                            </div>
+                        </div>
+                        <div className="event">
+                            <div className="left">
+
+                            </div>
+                            <div className="right info">
+                                <span className="dot"/>
+                                <div className="icon">
+                                    <Image src={"/icons/ceremony.png"} alt="ceremonia" unoptimized width={45}
+                                           height={45}/>
+                                </div>
+                                <div className="description">
+                                    <div className="time">18:00</div>
+                                    <span className="text">Ceremonia</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="event">
+                            <div className="left info">
+                                <div className="description">
+                                    <div className="time">18:30</div>
+                                    <span className="text">Życzenia</span>
+                                </div>
+                                <div className="icon">
+                                    <Image src={"/icons/wedding.png"} alt="wishes" unoptimized width={45} height={45}/>
+                                </div>
+                                <span className="dot"/>
+                            </div>
+                            <div className="right">
+
+                            </div>
+                        </div>
+                        <div className="event">
+                            <div className="left">
+
+                            </div>
+                            <div className="right info">
+                                <span className="dot"/>
+                                <div className="icon">
+                                    <Image src={"/icons/wine.png"} alt="wine" unoptimized width={45} height={45}/>
+                                </div>
+                                <div className="description">
+                                    <div className="time">18:40</div>
+                                    <span className="text">Przywitanie</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="event">
+                            <div className="left info">
+                                <div className="description">
+                                    <div className="time">18:45</div>
+                                    <span className="text">Przyjęcie</span>
+                                </div>
+                                <div className="icon">
+                                    <Image src={"/icons/party.png"} alt="party" unoptimized width={45} height={45}/>
+                                </div>
+                                <span className="dot"/>
+                            </div>
+                            <div className="right">
+
+                            </div>
+                        </div>
+                        <div className="event">
+                            <div className="left">
+
+                            </div>
+                            <div className="right info">
+                                <span className="dot"/>
+                                <div className="icon">
+                                    <Image src={"/icons/cake.png"} alt="tort" unoptimized width={45} height={45}/>
+                                </div>
+                                <div className="description">
+                                    <div className="time">21:00</div>
+                                    <span className="text">Tort</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="event">
+                            <div className="left info">
+                                <div className="description">
+                                    <div className="time">00:30</div>
+                                    <span className="text">Oczepiny!</span>
+                                </div>
+                                <div className="icon">
+                                    <Image src={"/icons/fashion.png"} alt="fashion" unoptimized width={45} height={45}/>
+                                </div>
+                                <span className="dot"/>
+                            </div>
+                            <div className="right">
+
+                            </div>
+                        </div>
+                        <div className="event">
+                            <div className="left">
+
+                            </div>
+                            <div className="right info">
+                                <span className="dot"/>
+                                <div className="icon">
+                                    <Image src={"/icons/landscape.png"} alt="landscape" unoptimized width={45}
+                                           height={45}/>
+                                </div>
+                                <div className="description">
+                                    <div className="time">5:00</div>
+                                    <span className="text">Dobranoc</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <p className="timeline-day">Niedziela</p>
+                    <div className="schedule">
+                        <div className="event-reverse">
+                            <div className="left info">
+                                <div className="description">
+                                    <div className="time small">9:00-10:30</div>
+                                    <span className="text">Śniadanie</span>
+                                </div>
+                                <div className="icon">
+                                    <Image src={"/icons/breakfast.png"} alt="breakfast" unoptimized width={45}
+                                           height={45}/>
+                                </div>
+                                <span className="dot"/>
+                            </div>
+                            <div className="right">
+                            </div>
+                        </div>
+                        <div className="event-reverse">
+                            <div className="left">
+                            </div>
+                            <div className="right info">
+                                <span className="dot"/>
+                                <div className="icon">
+                                    <Image src={"/icons/grill.png"} alt="grill" unoptimized width={45} height={45}/>
+                                </div>
+                                <div className="description">
+                                    <div className="time">14:00</div>
+                                    <span className="text">Grill</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="event-reverse">
+                            <div className="left info">
+                                <div className="description">
                                 <div className="time">18:30</div>
-                                <div className="description">Ceremonia</div>
-                            </div>
-                            <div className="timeline-event">
-                                <div className="icon"><Image src="/icons/party.png" alt="Fireworks" width={10}
-                                                             height={10} unoptimized/>
+                                    <span className="text">Kolacja</span>
                                 </div>
-                                <div className="time">19:00</div>
-                                <div className="description">Przyjęcie</div>
+                                <div className="icon">
+                                    <Image src={"/icons/salad.png"} alt="ceremonia" unoptimized width={45}
+                                           height={45}/>
+                                </div>
+                                <span className="dot"/>
                             </div>
-                            <div className="timeline-event">
-                                <div className="icon"><Image src="/icons/cake.png" alt="Wedding Ceremony" width={10}
-                                                             height={10} unoptimized/></div>
-                                <div className="time">21:00</div>
-                                <div className="description">Tort</div>
+                            <div className="right">
                             </div>
                         </div>
 
-                        <p className="timeline-day">Niedziela</p>
+                    </div>
 
-                        <div className="timeline">
+                    <p className="timeline-day">Poniedziałek</p>
+                    <div className="schedule">
+                        <div className="event">
+                            <div className="left">
 
-                            <div className="timeline-event">
-                                <div className="time">10:00</div>
-                                <div className="icon"><Image src="/icons/breakfast.png" alt="Wedding Lunch" width={10}
-                                                             height={10} unoptimized/>
+                            </div>
+                            <div className="right info">
+                                <span className="dot"/>
+                                <div className="icon">
+                                    <Image src={"/icons/breakfast.png"} alt="breakfast" unoptimized width={45}
+                                           height={45}/>
                                 </div>
-                                <div className="description">Śniadanie</div>
-                            </div>
-                            <div className="timeline-event">
-                                <div className="time">14:00</div>
-                                <div className="icon"><Image src="/icons/grill.png" alt="Cocktail Hour" width={10}
-                                                             height={10} unoptimized/></div>
-                                <div className="description">Grill</div>
-                            </div>
-                            <div className="timeline-event">
-                                <div className="time">18:00</div>
-                                <div className="icon"><Image src="/icons/salad.png" alt="Buffet Dinner" width={10}
-                                                             height={10} unoptimized/></div>
-                                <div className="description">Kolacja</div>
+                                <div className="description">
+                                    <div className="time small">8:30-10:00</div>
+                                    <span className="text">Śniadanie</span>
+                                </div>
                             </div>
                         </div>
-
-                        <p className="timeline-day">Poniedziałek</p>
-
-                        <div className="timeline">
-
-                            <div className="timeline-event">
-                                <div className="time">10:00</div>
-                                <div className="icon"><Image src="/icons/breakfast.png" alt="Wedding Lunch" width={10}
-                                                             height={10} unoptimized/>
+                        <div className="event mb-12">
+                            <div className="left info">
+                                <div className="description">
+                                    <div className="time">11:00</div>
+                                    <span className="text smaller">Wymeldowanie</span>
                                 </div>
-                                <div className="description">Śniadanie</div>
+                                <div className="icon">
+                                    <Image src={"/icons/away.png"} alt="away" unoptimized width={45} height={45}/>
+                                </div>
+                                <span className="dot"/>
                             </div>
-                            <div className="timeline-event">
-                                <div className="time">11:00</div>
-                                <div className="icon"><Image src="/icons/away.png" alt="Cocktail Hour" width={10}
-                                                             height={10} unoptimized/></div>
-                                <div className="description">Wymeldowanie</div>
+                            <div className="right">
+
                             </div>
                         </div>
-
                     </div>
                 </div>
             </div>
